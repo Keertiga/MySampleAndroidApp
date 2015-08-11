9ffe3257fdf078124fd4e661ddc24bfbbf458580
roobhini <roobhini@gmail.com>
2015-08-11 19:21:47 +0530

    Action bar changes
    Action bar changes

--- app/src/main/java/com/paypal/keraj/mysample/DisplayMessageActivity.java
+++ app/src/main/java/com/paypal/keraj/mysample/DisplayMessageActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.TextView;
 
@@ -31,7 +32,9 @@ public class DisplayMessageActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_display_message, menu);
-        return true;
+
+
+  return super.onCreateOptionsMenu(menu);
     }
 
     @Override
